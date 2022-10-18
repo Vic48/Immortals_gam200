85935398d4563d93d17977e7d8557a4493bbd206
Vic48 <97833997+Vic48@users.noreply.github.com>
2022-10-19 01:17:06 +0800

    Health Bar Player
    completed

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
     {
-        
+        slider.maxValue = health;
+        slider.value = health;
+
+        //value 1 ref to the left side colour
+        fill.color = gradient.Evaluate(1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHealth(int health)
     {
-        
+        slider.value = health;
+        // Change the value to go from 0 to 1
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
