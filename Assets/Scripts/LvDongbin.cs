284cdaa87fa05019cb3c5df6fc01a2adfa42a871
Vic48 <97833997+Vic48@users.noreply.github.com>
2022-10-31 03:53:30 +0800

    Change Script

--- Assets/Scripts/LvDongbin.cs
+++ Assets/Scripts/LvDongbin.cs
@@ -1,13 +1,58 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class LvDongbin : MonoBehaviour
 {
+    public Animator anim;
 
-    private void Start()
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 20;
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
+
+    void Update()
     {
+        if (Time.time > nextAttackTime)
+        {
+            //normal attck
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                normalAttack();
+                //0.5 second cool down time
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
     }
 
-    private void Update()
+    void normalAttack()
     {
+        //play an attack animation
+        anim.SetTrigger("NormalAttack");
+        //Detect enemy in the range attack 
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(
+            attackPoint.position,
+            attackRange,
+            enemyLayers
+        );
+        //Due Damage
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
     }
+
+    //draw stuff in editor to check attack range
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
 }
